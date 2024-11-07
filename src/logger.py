e5c38db984fa15fe9657e185d57763a2242d722f
Shaik Farooq <farooq20903@gmail.com>
2024-11-07 11:06:56 +0530

    added  modules

---
+++ src/logger.py
@@ -0,0 +1,19 @@
+##logging is crucial for tracking events that happen when application
+## which help in debugging and monitoring the application behavior
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE =F"{datetime.now().strftime('%m_%d_%Y"_%H_%M_%s')}.log"
+
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+  filename=LOG_FILE_PATH,
+  format="[%(asctime)s]%(lineno)d %(name)s - %(levelname)s - %(message)s",
+  level=logging.INFO
+)
